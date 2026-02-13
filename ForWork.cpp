79f8725ad5d56fcf8e5028ef12b0f1d2b5bbb0bd
laijara <fireman0123212@gmail.com>
2026-02-13 23:01:15 +0300

    Добавьте файлы проекта.

---
+++ ForWork.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void StartProgramm();
+int SecurityInput(int);
+void openAllAutoStart();
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	StartProgramm();
+	return 0;
+}
+
+void StartProgramm() {
+	int inp, SecInp;
+	while (true)
+	{
+		cout << "0. Выход" << endl;
+		cout << "1. Открыть папку автозагрузки" << endl;
+		cout << "Выберите: ";
+		cin >> inp;
+		SecInp = SecurityInput(inp);
+		if (SecInp != -1) {
+			if (SecInp == 0) {
+				break;
+			}
+			system("cls");
+			openAllAutoStart();
+		}
+		else {
+			system("cls");
+			cout << "Некорректный ввод" << endl << endl;
+			continue;
+		}
+	}
+}
+
+int SecurityInput(int inpp) {
+	if (!cin.fail() && inpp == 0 || inpp == 1) {
+		return inpp;
+	}
+	else {
+		cin.clear();
+		cin.ignore();
+		return -1;
+	}
+}
+
+void openAllAutoStart() {
+	system("explorer \"%ProgramData%\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\"");
+}
